4d8c8375669e2b53feb53a9adf54663423a6eec1
lerick80 <lerick80@gmail.com>
2026-09-23 19:27:35 -0600

    cambio de src

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -195,7 +195,7 @@ export default function Home() {
                         <p className="text-[#523324]">{hora}</p>
                       </div>
                     ) : (
-                      <img src={`../images/icons/anillos.png`} alt={evento} className="w-16 h-16 object-contain" />
+                      <img src={`/images/icons/anillos.png`} alt={evento} className="w-16 h-16 object-contain" />
                     )}
                   </div>
 
@@ -203,7 +203,7 @@ export default function Home() {
 
                   <div className="w-[42%] flex justify-start">
                     {textoIzquierda ? (
-                      <img src={`../images/icons/${icono}.png`} alt={evento} className="w-16 h-16 object-contain" />
+                      <img src={`/images/icons/anillos.png`} alt={evento} className="w-16 h-16 object-contain" />
                     ) : (
                       <div className="text-left">
                         <p className="text-lg text-[#c98a5e]" style={{ fontFamily: "Cormorant Garamond" }}>{evento}</p>
