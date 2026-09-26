1d6e54563e3e10419967cbc5d5c7a30681dfb8d7
lerick80 <lerick80@gmail.com>
2026-09-25 21:26:06 -0600

    Add padres

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -116,13 +116,14 @@ export default function Home() {
       </section>
 
       {/* padres */}
-      <section className="py-16 px-6 text-center max-w-md mx-auto border-t border-[#e4d8c6]">
+      <section className="py-16 px-6 text-center max-w-md mx-auto">
         <FadeIn>
           <h3 className="text-xs tracking-widest uppercase text-[#6e2c3a] mb-4">
             Con la bendición de Dios y en agradecimiento a nuestros padres
           </h3>
           <p className="text-lg">Sonia González Cerritos</p>
           <p className="text-lg">Sergio Pérez Orduña</p>
+          <br/>
           <p className="text-lg mt-1">Josefina González Reyes</p>
           <p className="text-lg">Florencio Rendón Hernández</p>
         </FadeIn>
